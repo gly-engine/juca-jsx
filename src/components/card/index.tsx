12afe6455a0f14d2510d8a4a80571f67d9bfd474
Joao Vicente <joaorovera73@gmail.com>
2026-01-23 17:38:58 -0300

    feat: create a card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,43 @@
+import { GlyApp, GlyStd } from "@gamely/gly-types";
+import { Button, JucaButtonProperties } from "../button";
+import { AcaiTextBlockProperties, Text, TextBlock } from "@gamely/acai-jsx/basics/text";
+import { Icon, JucaIconProperties } from "../icon";
+
+export type JucaCardProperties = JucaButtonProperties 
+  & JucaIconProperties & {
+    title?: AcaiTextBlockProperties["content"];
+    description?: AcaiTextBlockProperties["content"];
+  }
+  & Pick<
+  AcaiTextBlockProperties,
+  "content" | "color" | "font_size" | "font_name" | "align" | "valign"
+> & {
+  span?: number;
+  offset?: number;
+  after?: number;
+  style?: string;
+};
+
+export function Card(props: JucaCardProperties, std: GlyStd) {
+  const title = props.title ?? "";
+  const description = props.description ?? "";
+
+  return (
+    <item
+      style={props.style}
+      offset={props.offset}
+      span={props.span ?? 1}
+    >
+      <node>
+        <Button {...props}></Button>
+        <grid class="3x1">
+          <Icon src={props.src}></Icon>
+          <grid class="1x3" span={2}>
+            <TextBlock content={title} {...props}></TextBlock>
+            <TextBlock span={2} content={description} {...props}></TextBlock>
+          </grid>
+        </grid>
+      </node>
+    </item>
+  )
+}
