7958e666d07011dfb1f74b3258b4a1eef2fd3044
Joao Vicente <joaorovera73@gmail.com>
2026-01-23 17:38:41 -0300

    feat: implement icon component

--- src/components/button/icon.tsx
+++ src/components/button/icon.tsx
@@ -1,9 +1,10 @@
-import type { GlyStd, GlyApp } from "@gamely/gly-types";
-import { Button, AcaiButtonProperties } from "./index";
+import type { GlyStd } from "@gamely/gly-types";
+import { Button, JucaButtonProperties } from "./index";
+import { Icon } from "../icon";
 
 export type IconPosition = "center" | "left" | "right" | "top" | "bottom";
 
-export type IconButtonProperties = AcaiButtonProperties & {
+export type IconButtonProperties = JucaButtonProperties & {
   src: string;
   icon_position?: IconPosition;
   icon_color?: number | (() => number);
@@ -12,14 +13,7 @@ export type IconButtonProperties = AcaiButtonProperties & {
 
 export function IconButton(props: IconButtonProperties, std: GlyStd) {
   const icon_position = props.icon_position ?? "center";
-  const spacing = props.spacing ?? 8;
-  const x_pos = props.x ?? 0;
-  const y_pos = props.y ?? 0;
   const src = props.src;
-
-  const getX = typeof x_pos !== "function" ? () => x_pos : x_pos;
-  const getY = typeof y_pos !== "function" ? () => y_pos : y_pos;
-  const getSrc = typeof src !== "function" ? () => src : src;
 
   return (
     <item
@@ -27,44 +21,10 @@ export function IconButton(props: IconButtonProperties, std: GlyStd) {
       offset={props.offset}
       span={props.span ?? 1}
     >
-      <node
-        load={() => <Button border_width={0} {...props} />}
-        post_draw={(self: GlyApp["data"]) => {
-
-          const btnWidth = self.width;
-          const btnHeight = self.height;
-
-          // const imgWidth = std.image.mensure_width(getSrc());
-          // const imgHeight = std.image.mensure_height(getSrc());
-
-          const imgWidth = 80
-          const imgHeight = 80
-
-
-          const btnX = getX();
-          const btnY = getY();
-          const centerX = btnX + btnWidth / 2;
-          const centerY = btnY + btnHeight / 2;
-
-          let iconX = centerX - imgWidth / 2;
-          let iconY = centerY - imgHeight / 2;
-
-          if (icon_position === "left") {
-            iconX = btnX + spacing;
-            iconY = centerY - imgHeight / 2;
-          } else if (icon_position === "right") {
-            iconX = btnX + btnWidth - imgWidth - spacing;
-            iconY = centerY - imgHeight / 2;
-          } else if (icon_position === "top") {
-            iconX = centerX - imgWidth / 2;
-            iconY = btnY + spacing;
-          } else if (icon_position === "bottom") {
-            iconX = centerX - imgWidth / 2;
-            iconY = btnY + btnHeight - imgHeight - spacing;
-          }
-          std.image.draw(getSrc(), iconX, iconY);
-        }}
-      />
+      <node>
+          <Button border_width={0} {...props} />
+          <Icon src={src} position={icon_position} />
+      </node>
     </item>
   );
 }
---
+++ src/components/icon.tsx
@@ -0,0 +1,67 @@
+import type { GlyStd, GlyApp } from "@gamely/gly-types";
+
+export type IconPosition = "center" | "left" | "right" | "top" | "bottom";
+export type IconSize = "fill" | "contain" | "cover";
+
+export type JucaIconProperties = {
+  src: string | (() => string);
+  x?: number | (() => number);
+  y?: number | (() => number);
+  position?: IconPosition;
+  size?: IconSize;
+} & {
+  span?: number;
+  offset?: number;
+  after?: number;
+  style?: string;
+};
+
+export function Icon(props: JucaIconProperties, std: GlyStd) {
+  const src = props.src;
+  const x_pos = props.x ?? 0;
+  const y_pos = props.y ?? 0;
+  const position = props.position ?? "center";
+
+  const getSrc = typeof src !== "function" ? () => src : src;
+  const getX = typeof x_pos !== "function" ? () => x_pos : x_pos;
+  const getY = typeof y_pos !== "function" ? () => y_pos : y_pos;
+
+  return (
+    <item
+      style={props.style}
+      offset={props.offset}
+      span={props.span ?? 1}
+    >
+      <node
+        draw={(self: GlyApp["data"]) => {
+          const srcPath = getSrc();
+          
+          const imgWidth = std.image.mensure_width(getSrc());
+          const imgHeight = std.image.mensure_height(getSrc());
+
+          let iconX = getX();
+          let iconY = getY();
+
+          if (position === "center") {
+            iconX = (self.width - imgWidth) / 2;
+            iconY = (self.height - imgHeight) / 2;
+          } else if (position === "left") {
+            iconX = 0;
+            iconY = (self.height - imgHeight) / 2;
+          } else if (position === "right") {
+            iconX = self.width - imgWidth;
+            iconY = (self.height - imgHeight) / 2;
+          } else if (position === "top") {
+            iconX = (self.width - imgWidth) / 2;
+            iconY = 0;
+          } else if (position === "bottom") {
+            iconX = (self.width - imgWidth) / 2;
+            iconY = self.height - imgHeight;
+          }
+
+          std.image.draw(srcPath, iconX, iconY);
+        }}
+      />
+    </item>
+  );
+}
